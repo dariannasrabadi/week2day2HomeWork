f0c886a68c2ec7d42c71fae78329c78b1330c4d7
Darian Nasrabadi <darian.nasrabadi@gmail.com>
2017-12-15 18:07:34 -0600

    add highlighter function to select row(s) and click delete to remove them

--- salary.js
+++ salary.js
@@ -2,7 +2,8 @@ $(document).ready(onReady);
 
 function onReady(){//Function full of listeners
 $('#submitButton').on('click', employeeSubmited);
-
+$('tbody').on('click', '.tbodyRow', employeeHighlighted);
+$('#deleteButton').on('click', removeEmployee);
 }
 
 class Employee { // start class
@@ -13,11 +14,14 @@ class Employee { // start class
     this.id = id;
     this.job = job;
     this.salary = salary;
+    this.status = 'Employed';
   }// end class constructor
 } // end of class
 var employee = []; // storage of employees array
-var combinedSalaries = 0; // storage of all employee salaries
-function employeeSubmited(){ //START OF employeeSubmited function
+var combinedSalaries = 130000; // storage of all employee salaries (130000 is the initial one on the HTML)
+
+/****START OF employeeSubmited function****/
+function employeeSubmited(){
 var firstName = $('#firstNameInput').val();
 var lastName = $('#lastNameInput').val();
 var id = $('#idInput').val();
@@ -26,11 +30,15 @@ var salary = $('#salaryInput').val();
 
 employee.push(new Employee(firstName, lastName, id, job, salary)); // Internal log of all employees previous and current
 
-var $row = $('<tr>');
+var $row = $('<tr class="tbodyRow">');
 
-if (firstName === ('') || lastName === ('') || id === ('') || job === ('') || salary === ('')) { //TEMP IF STATEMENT FOR MAKING SURE ALL ARE FILLED. WILL ADD LATER
+if (firstName === ('') || lastName === ('') || id === ('') || job === ('') || salary === ('')) {// So that no fields are empty.
   alert('Please fill out all information');
-}// end if statement
+}
+else if (salary < 0) { //Salary cant be less than 0. 0 is free so yay free work.
+  alert('Please provide proper salary');
+  $('#salaryInput').val('').focus();//empty the value and focus on it
+}
 else { //start else
 $row.append('<td class="ifDeleted">' + firstName + '</td>'); //Appending it to the TR
 $row.append('<td class="ifDeleted">' + lastName + '</td>'); // Appending it to TR
@@ -44,4 +52,15 @@ $('#tableFootCell').empty().append(combinedSalaries);
 $('input').val('');
 console.log(employee);
 }//end else
+}// End employee submitted function
+function employeeHighlighted(){ //start
+  //When a td is clicked the parent row its in will have a class added.
+  //then that class background color will change to indicate it has been selected.
+  // this way there can be multiple employees removed.
+$(this).toggleClass('highlighted');
+}
+
+function removeEmployee(){
+// remove the TR of the highlighted classes!
+  $('.highlighted').remove();
 }
