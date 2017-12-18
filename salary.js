c49f19518ad6610fff118b65f0b23077f58eec63
Darian Nasrabadi <darian.nasrabadi@gmail.com>
2017-12-18 00:24:14 -0600

    add final change for pro mode. Made a one time use that removes the right employee data but breaks after. Other changes done as well.

--- salary.js
+++ salary.js
@@ -19,7 +19,7 @@ class Employee { // start class
     this.id = id;
     this.job = job;
     this.salary = salary;
-    this.status = 'Employed';
+    // this.status = 'Employed'; // Have to experiment more to find a way to change the specific highlighted employees status once deleted so commenting this out for now.
   }// end class constructor
 } // end of class
 var employee = []; // storage of employees array
@@ -45,28 +45,42 @@ else if (salary < 0) { //Salary cant be less than 0. 0 is free so yay free work.
   $('#salaryInput').val('').focus();//empty the value and focus on it
 }
 else { //start else
-$row.append('<td class="ifDeleted">' + firstName + '</td>'); //Appending it to the TR
-$row.append('<td class="ifDeleted">' + lastName + '</td>'); // Appending it to TR
-$row.append('<td class="ifDeleted">' + id + '</td>'); // Appending it to TR
-$row.append('<td class="ifDeleted">' + job + '</td>'); // Appending it to TR
-$row.append('<td>' + salary + '</td>'); // Appending it to TR
+$row.append('<td class="fC">' + firstName + ' ' + '</td>'); //Appending it to the TR & adding space for data portion.
+$row.append('<td class="lC">' + lastName + ' ' + '</td>'); // Appending it to TR
+$row.append('<td class="iC">' + id + ' ' + '</td>'); // Appending it to TR
+$row.append('<td class="jC">' + job + ' ' + '</td>'); // Appending it to TR
+$row.append('<td class="sC">' + salary + '</td>'); // Appending it to TR
 $('tbody').append($row); // Appending to table body
 combinedSalaries += (parseInt(salary)/12);
-
+// HARD & PRO mode instructions were confusing. It says dont need to change the removed employees salary on total then it says on pro to remove it form data. Really confused.
 $('#tableFootCell').empty().append(combinedSalaries);
 
+/*TESTING .data() HERE*/
+var employeeActiveData;
+$('table').data(('ActiveEmployee ' + $('tbody tr').length), firstName+' '+lastName+' '+id+' '+job+' '+salary); //Adding all new employees to this.
+
+console.log($('table').data());
+/*END of .data() HERE*/
+
 $('input').val('');
-console.log(employee);
+console.log('array of employees:', employee);
 }//end else
 }// End employee submitted function
 function employeeHighlighted(){ //start
   //When a td is clicked the parent row its in will have a class added.
   //then that class background color will change to indicate it has been selected.
   // this way there can be multiple employees removed.
-$(this).toggleClass('highlighted');
+$(this).children().toggleClass('highlighted'); //made the TD highlighted instead.
 }
-
+var i = 1; // for retired employee count.
 function removeEmployee(){
+/*Additional .data() information*/
+  $('table').data(('RetiredEmployee ' + i), $('.highlighted').text()); //grabs data from the cells removed.
+  $('table').removeData(('ActiveEmployee ' + $('.highlighted').closest('tr').index())); // able to work only for the first removed name. then its broken, need to learn of a proper way.
+console.log($('table').data());
+i++;
 // remove the TR of the highlighted classes!
-  $('.highlighted').remove();
+  $('.highlighted').closest('tr').remove(); //removing parents of the highlighted.
+
+
 }
